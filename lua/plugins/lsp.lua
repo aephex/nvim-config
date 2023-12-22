10e25685dfbd75efc6961d17e2c1c625baecff3e
aephex <24578+aephex@users.noreply.github.com>
2023-12-22 12:57:29 -0500

    Switch to none-ls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -174,7 +174,7 @@ return {
     end,
   },
   {
-    "jose-elias-alvarez/null-ls.nvim",
+    "nvimtools/none-ls.nvim",
     event = { "BufReadPre", "BufNewFile" },
     lazy = true,
     config = function()
